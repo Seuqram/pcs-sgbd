fad93df484027ca341d4d2d9bc6de7876653bcd0
Rodrigo Marques <rodrigo.marques@uniriotec.br>
2017-11-21 16:28:15 -0200

    Inicializacao do repositorio

---
+++ src/main/java/com/academia/bir/AcademiaBirApplication.java
@@ -0,0 +1,12 @@
+package com.academia.bir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaBirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaBirApplication.class, args);
+	}
+}
